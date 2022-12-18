984d509c247efe36483c8b578af49f6587df6bcf
Malak010 <malakabdullah005@gmail.com>
2022-12-18 12:20:33 +0400

    MyUtility

---
+++ src/LargestNumber.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-3
+ *
+ */
+public class LargestNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
